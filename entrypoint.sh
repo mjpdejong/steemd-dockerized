d8a3e9463892aa7dcd2637fb9218d21439632e75
Marco <mail@mjpdejong.com>
2016-08-09 20:10:51 +0200

    Provided via -v when starting docker -- see readme

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir -p '/steemdata/witness_node_data_dir'
-cp '/usr/local/steem/config.ini' '/steemdata/witness_node_data_dir/'
+#mkdir -p '/steemdata/witness_node_data_dir'
+#cp '/usr/local/steem/config.ini' '/steemdata/witness_node_data_dir/'
 cd '/steemdata'
 exec steemd "$@"
